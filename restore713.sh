44be90cd816287f374169e0c4127208acfd6943d
80036nd <40246027+80036nd@users.noreply.github.com>
2019-11-11 17:14:36 -0500

    Update restore713.sh

--- restore713.sh
+++ restore713.sh
@@ -9,4 +9,6 @@ chmod +x ~/Documents/713Update/tools/ipwndfu/image3.py
 chmod +x ~/Documents/713Update/tools/ipwndfu/ipwndfu
 chmod +x ~/Documents/713Update/tools/ipwndfu/ipwnrecovery
 chmod +x tools/idevicerestoreLHTSS
+chmod +x ~/Documents/713Update/tss_server/run.sh
+chmod +x ~/Documents/713Update/tss_server/tss.php
 ./restore.sh ~/Downloads/iPhone3,3_7.1.3_11D259_Restore.ipsw
