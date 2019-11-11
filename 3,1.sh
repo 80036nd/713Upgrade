a8210ed0c0cfb1ffe853069e0a46a45df31e8e90
80036nd <40246027+80036nd@users.noreply.github.com>
2019-11-11 16:11:18 -0500

    Update and rename 3,1.sh to makeipsw.sh

--- 3,1.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -e
-cd ~/Documents/713Update
-./make_ipsw.sh ~/Downloads/iPhone3\,1_7.1.2_11D257_Restore.ipsw
---
+++ makeipsw.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -e
+cd ~/Documents/713Update
+./make_ipsw.sh ~/Downloads/iPhone3\,3_7.1.2_11D257_Restore.ipsw 
