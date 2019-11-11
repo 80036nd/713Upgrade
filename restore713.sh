1b70c7742d835f2b4c82a0e854e358d5a188af23
80036nd <40246027+80036nd@users.noreply.github.com>
2019-11-11 17:10:00 -0500

    Update restore713.sh

--- restore713.sh
+++ restore713.sh
@@ -1,4 +1,11 @@
 #!/bin/bash -e
 cd ~/Documents/713Update
 chmod +x ./restore.sh
+chmod +x ~/Documents/713Update/tools/ipwndfu/alloc8.py
+chmod +x ~/Documents/713Update/tools/ipwndfu/dfu.py
+chmod +x ~/Documents/713Update/tools/ipwndfu/dfuexec.py
+chmod +x ~/Documents/713Update/tools/ipwndfu/image3_24Kpwn.py
+chmod +x ~/Documents/713Update/tools/ipwndfu/image3.py
+chmod +x ~/Documents/713Update/tools/ipwndfu/ipwndfu
+chmod +x ~/Documents/713Update/tools/ipwndfu/ipwnrecovery
 ./restore.sh ~/Downloads/iPhone3,3_7.1.3_11D259_Restore.ipsw
