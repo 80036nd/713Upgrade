1f3d1dacb94980615e04dc5fbdc776a04c847394
80036nd <40246027+80036nd@users.noreply.github.com>
2019-11-11 16:21:27 -0500

    Update makeipsw.sh

--- makeipsw.sh
+++ makeipsw.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
 cd ~/Documents/713Update
 chmod +x ./make_ipsw.sh
-chmod +x scripts/01_createIPSW.sh
+chmod +x /scripts/01_createIPSW.sh
+chmod +x /tools/partialZipBrowser
 ./make_ipsw.sh ~/Downloads/iPhone3\,3_7.1.2_11D257_Restore.ipsw 
